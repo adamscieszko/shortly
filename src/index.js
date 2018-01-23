c2aea5aa571cce1650bded4265d2de6aed97cf0b
Adam Ścieszko <adam.scieszko@gmail.com>
2018-01-23 21:27:41 +0100

    Added Link and LinkList components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    link: new HttpLink('https://api.graph.cool/simple/v1/cjcs1zksp3wo80153fcgk94q3'),
+    cache: new InMemoryCache()
+});
+
+const withApolloProvider = Comp => (
+    <ApolloProvider client={client}>{Comp}</ApolloProvider>
+);
+
+ReactDOM.render(
+    withApolloProvider(<App />),
+    document.getElementById('root')
+);
 registerServiceWorker();
